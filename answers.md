02e16e3b226066dd3b0f606c80f758af6b9842da
bxqm803 <142374785+bxqm803@users.noreply.github.com>
2024-04-17 01:55:27 +0000

    assignment3

--- answers.md
+++ answers.md
@@ -1,8 +1,19 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:**_______Mengzhou Wang__________________
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+1.a,  for each time, find the largest number that is power of 2 but smaller then n. Since all coins is power of 2, it's include all value. In this way, it can produce as few coins as possible that sum to N
+
+1.b, For each time, choosing the largest coin(2^k) can help us find the optmal choice for this substructure,which is we using the fewest coins to do that. Each time we find the largest coin value is a substructure for whole problem. For each substructure, we have optaml substructure properties can make whole solution is optmal.
+
+1.c the work and span are both O(logn)
+
+2.a for example, the denominations has coin 1, 3, 4, 5 if n=7 and we use same greedy algorithm. we will get the coin 5, 1, 1, but the optmal choice is 3, 4.
+
+2.b optmal structure property in this problems is find possible solution that can get N if we add one more coin. To find the minimum coins we use for those possible solution is finding optmal structure. And each substructure have has to find their optmal substructure until we use 0 coin.
+
+3.c work is O(n) span is O(n)
--- main.py
+++ main.py
@@ -20,10 +20,52 @@ def MED(S, T):
 
 
 def fast_MED(S, T, MED={}):
+  if (S, T) in MED:
+    return MED[(S, T)]
+
+  if S == "":
+    return len(T)
+
+  elif T == "":
+    return len(S)
+  elif S[0] == T[0]:
+    result = fast_MED(S[1:], T[1:], MED)
+  else:
+    insert = fast_MED(S, T[1:], MED)
+    delete = fast_MED(S[1:], T, MED)
+    result = 1 + min(insert, delete)
+    MED[(S, T)] = result
+  return result
     # TODO -  implement top-down memoization
-    pass
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+  return fast_align_MED_(S, T, MED={})[1:]
 
+def fast_align_MED_(S, T, MED={}):
+  if (S, T) in MED:
+    return MED[(S, T)]
+
+  if S == "":
+    return len(T), len(T) * '-', T
+
+  elif T == "":
+    return len(S), len(S) * '-', S
+  elif S[0] == T[0]:
+    result = fast_align_MED_(S[1:], T[1:], MED)[0], S[0] + fast_align_MED_(S[1:], T[1:], MED)[1], T[0] + fast_align_MED_(S[1:], T[1:], MED)[2]
+  else:
+    insert = fast_align_MED_(S, T[1:], MED)[0]
+    delete = fast_align_MED_(S[1:], T, MED)[0]
+    if insert<=delete:
+      result=1+fast_align_MED_(S, T[1:], MED)[0], "-" + fast_align_MED_(S, T[1:], MED)[1], T[0] + fast_align_MED_(S, T[1:], MED)[2]
+    else :
+      result=1+fast_align_MED_(S[1:], T, MED)[0], S[0] + fast_align_MED_(S[1:], T, MED)[1], "-" + fast_align_MED_(S[1:], T, MED)[2]
+    MED[(S, T)] = result
+  return result
+
+
+
+print(fast_align_MED('book', 'back'))
+print(fast_align_MED('kookaburra', 'kookybird'))
+print(fast_align_MED('relevant', 'elephant'))
+print(fast_align_MED('AAAGAATTCA', 'AAATCA'))
+# I think result is correct but different format
